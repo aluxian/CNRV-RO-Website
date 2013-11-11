6275cbe85c34b789a43741a4fb5666ab9c4cf93d
Aluxian <rosian_alex@yahoo.com>
2013-11-11 17:04:40 +0200

    Changed home page to Posts.index

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -58,5 +58,3 @@ var Main = function () {
 };
 
 exports.Main = Main;
-
-
--- config/router.js
+++ config/router.js
@@ -16,30 +16,12 @@
  *
 */
 
-
 var router = new geddy.RegExpRouter();
 
-router.get('/').to('Main.index');
+// Main
+router.get('/').to('Posts.index');
 
-// Basic routes
-// router.match('/moving/pictures/:id', 'GET').to('Moving.pictures');
-//
-// router.match('/farewells/:farewelltype/kings/:kingid', 'GET').to('Farewells.kings');
-//
-// Can also match specific HTTP methods only
-// router.get('/xandadu').to('Xanadu.specialHandler');
-// router.del('/xandadu/:id').to('Xanadu.killItWithFire');
-//
-// Resource-based routes
-// router.resource('hemispheres');
-//
-// Nested Resource-based routes
-// router.resource('hemispheres', function(){
-//   this.resource('countries');
-//   this.get('/print(.:format)').to('Hemispheres.print');
-// });
-
-
+// Auth
 router.get('/login').to('Main.login');
 router.get('/logout').to('Main.logout');
 router.post('/auth/local').to('Auth.local');
@@ -49,7 +31,5 @@ router.get('/auth/facebook').to('Auth.facebook');
 router.get('/auth/facebook/callback').to('Auth.facebookCallback');
 router.get('/auth/yammer').to('Auth.yammer');
 router.get('/auth/yammer/callback').to('Auth.yammerCallback');
-router.resource('users');
-router.resource('posts');
-router.resource('comments');
+
 exports.router = router;
