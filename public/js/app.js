4193c656d2b35e508e591f9e3dbdbb2d0cce9f7b
Aluxian <rosian_alex@yahoo.com>
2014-04-27 00:04:32 +0300

    Fixes menu items click

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,20 @@
 $(function() {
   // Run the WYSIWYG editor
-  // TODO: Only run when user is on an edit page
   $('#editor').wysiwyg();
 
   // Prevent the input from closing the dropdown
-  // TODO: same as above
   $('#add-link-input, #add-pic-input').click(function(event) {
     event.stopPropagation();
   });
 
   // Copy content from the editor box to a hidden input to be sent as a POST param
-  // TODO only run on edit page
   $('#transferContent').click(function(event) {
     $('#postValue').val($('#editor').html());
+  });
+
+  // Disable bootstrap's menu item click handler
+  $('.disabled-open').click(function(event) {
+    event.preventDefault();
   });
 });
 
