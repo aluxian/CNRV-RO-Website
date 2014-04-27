4dc0d762116654f5c6ba28a04fc551823f50fed5
Aluxian <rosian_alex@yahoo.com>
2014-04-27 12:20:06 +0300

    Adds event model

---
+++ app/models/event.js
@@ -0,0 +1,8 @@
+var Event = function () {
+  this.defineProperties({
+    name: {type: 'string', required: true}
+    name: {type: 'date', required: true}
+  });
+};
+
+Event = geddy.model.register('Event', Event);
