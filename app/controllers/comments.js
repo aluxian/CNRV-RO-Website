55684fbce2b4f56ffa5bb06744f968d3fa2d063d
Aluxian <rosian_alex@yahoo.com>
2014-05-05 20:03:13 +0300

    Fixes comments create controller bug

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -7,11 +7,22 @@ var Comments = function () {
   this.before(security.userHasAccess, { only: ['remove'], async: true });
   this.respondsWith = ['html', 'json'];
 
-  this.create = utils.defaultCreate.bind(this
-    , {controller: 'Posts', id: params.postId}
-    , 'Comentariu invalid.'
-    , 'Comentariul a fost adăugat.'
-  );
+  this.create = function(req, resp, params) {
+    var self = this
+      , model = geddy.model.Comment.create(params);
+
+    if (!model.isValid()) {
+      self.flash.error('Comentariu invalid.');
+      self.redirect({controller: 'Posts', id: params.postId});
+    } else {
+      model.save(function(err) {
+        if (err) { throw err; }
+
+        self.flash.success('Comentariul a fost adăugat.');
+        self.redirect({controller: 'Posts', id: params.postId});
+      });
+    }
+  };
 
   this.remove = utils.defaultRemove.bind(this, true, 'Comentariul a fost șters.');
 
